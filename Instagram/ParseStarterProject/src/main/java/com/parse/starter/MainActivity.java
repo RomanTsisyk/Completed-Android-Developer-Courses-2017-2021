7bcf86b2d8ac5c1e0468ec4ce13f8ea80d269b6a
romantsisyk <romantsisyk1@gmail.com>
2017-01-14 19:39:02 +0100

    Conecting app UI and Parse Server

--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,6 +11,11 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -27,71 +32,108 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Boolean signUpModeActive = true;
+
+    TextView changeSignupModeTextView;
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.changeSignupModeTextView) {
+
+            Button signupButton = (Button) findViewById(R.id.signupButton);
+
+            if (signUpModeActive) {
+
+                signUpModeActive = false;
+                signupButton.setText("Login");
+                changeSignupModeTextView.setText("Or, Signup");
+
+            } else {
+
+                signUpModeActive = true;
+                signupButton.setText("Signup");
+                changeSignupModeTextView.setText("Or, Login");
+
+            }
+
+        }
+
+    }
+
+    public void signUp(View view) {
+
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+
+            Toast.makeText(this, "A username and password are required.", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            if (signUpModeActive) {
+
+                ParseUser user = new ParseUser();
+
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+
+                            Log.i("Signup", "Successful");
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+            } else {
+
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+
+                        if (user != null) {
+
+                            Log.i("Signup", "Login successful");
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
+
+
+            }
+        }
+
+
+    }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-        ParseUser.logOut();
+      changeSignupModeTextView = (TextView) findViewById(R.id.changeSignupModeTextView);
 
-        if (ParseUser.getCurrentUser() != null) {
-
-            Log.i("currentUser", "User logged in " + ParseUser.getCurrentUser().getUsername());
-
-        } else {
-
-            Log.i("currentUser", "User not logged in");
-
-        }
+      changeSignupModeTextView.setOnClickListener(this);
 
 
-
-       /*
-
-      ParseUser.logInInBackground("robpercival", "asdf", new LogInCallback() {
-          @Override
-          public void done(ParseUser user, ParseException e) {
-
-              if (user != null) {
-
-                  Log.i("Login", "Successful");
-
-              } else {
-
-                  Log.i("Login", "Failed: " + e.toString());
-
-              }
-
-          }
-      });
-
-
-
-
-      ParseUser user = new ParseUser();
-
-      user.setUsername("robpercival");
-      user.setPassword("myPass");
-
-      user.signUpInBackground(new SignUpCallback() {
-          @Override
-          public void done(ParseException e) {
-
-              if (e == null) {
-
-                  Log.i("Sign Up", "Successful");
-
-              } else {
-
-                  Log.i("Sign Up", "Failed");
-
-              }
-
-          }
-      });
-*/
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
