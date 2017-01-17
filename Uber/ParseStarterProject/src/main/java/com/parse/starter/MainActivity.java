7a5d7c9110a81265de22ccc6e8d32abfe8fe7254
romantsisyk <romantsisyk1@gmail.com>
2017-01-17 18:33:55 +0100

    Creating Rider Activity

--- Uber/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Uber/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -7,46 +7,108 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 package com.parse.starter;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Switch;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
-import com.parse.SignUpCallback;
-
-import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
+
+  public void redirectActivity() {
+
+    if (ParseUser.getCurrentUser().getString("riderOrDriver").equals("rider")) {
+
+        Intent intent = new Intent(getApplicationContext(), RiderActivity.class);
+        startActivity(intent);
+
+    }
+  }
+
+  public void getStarted(View view) {
+
+    Switch userTypeSwitch = (Switch) findViewById(R.id.userTypeSwitch);
+
+    Log.i("Switch value", String.valueOf(userTypeSwitch.isChecked()));
+
+    String userType = "rider";
+
+    if (userTypeSwitch.isChecked()) {
+
+      userType = "driver";
+
+    }
+
+    ParseUser.getCurrentUser().put("riderOrDriver", userType);
+
+      ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+          @Override
+          public void done(ParseException e) {
+
+              redirectActivity();
+
+          }
+      });
+
+
+
+
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    getSupportActionBar().hide();
+
+
+
+    if (ParseUser.getCurrentUser() == null) {
+
+      ParseAnonymousUtils.logIn(new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+
+          if (e == null) {
+
+            Log.i("Info", "Anonymous login successful");
+
+          } else {
+
+            Log.i("Info", "Anonymous login failed");
+
+          }
+
+
+        }
+      });
+
+    } else {
+
+      if (ParseUser.getCurrentUser().get("riderOrDriver") != null) {
+
+        Log.i("Info", "Redirecting as " + ParseUser.getCurrentUser().get("riderOrDriver"));
+
+        redirectActivity();
+
+      }
+
+
+    }
+
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
-
 
 }
