9e6be9a9b1a57e028183662588cc7a708d019772
romantsisyk <romantsisyk1@gmail.com>
2017-01-21 20:27:28 +0100

    Scrolling

--- FlappyBird/core/src/com/robpercival/flappybird/FlappyBird.java
+++ FlappyBird/core/src/com/robpercival/flappybird/FlappyBird.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -25,6 +26,9 @@ public class FlappyBird extends ApplicationAdapter {
     float birdY = 0;
     float velocity = 0;
     Circle birdCircle;
+    int score = 0;
+    int scoringTube = 0;
+    BitmapFont font;
 
     int gameState = 0;
     float gravity = 2;
@@ -50,6 +54,9 @@ public class FlappyBird extends ApplicationAdapter {
 		background = new Texture("bg.png");
         //shapeRenderer = new ShapeRenderer();
         birdCircle = new Circle();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(10);
 
         birds = new Texture[2];
         birds[0] = new Texture("bird.png");
@@ -89,6 +96,24 @@ public class FlappyBird extends ApplicationAdapter {
 
         if (gameState != 0) {
 
+            if (tubeX[scoringTube] < Gdx.graphics.getWidth() / 2) {
+
+                score++;
+
+                Gdx.app.log("Score", String.valueOf(score));
+
+                if (scoringTube < numberOfTubes - 1) {
+
+                    scoringTube++;
+
+                } else {
+
+                    scoringTube = 0;
+
+                }
+
+            }
+
             if (Gdx.input.justTouched()) {
 
                 velocity = -30;
@@ -105,6 +130,8 @@ public class FlappyBird extends ApplicationAdapter {
                 } else {
 
                     tubeX[i] = tubeX[i] - tubeVelocity;
+
+
 
                 }
 
@@ -143,9 +170,11 @@ public class FlappyBird extends ApplicationAdapter {
 
 
         batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
-        batch.end();
+
+        font.draw(batch, String.valueOf(score), 100, 200);
 
         birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
+
 
 
         //shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -166,6 +195,7 @@ public class FlappyBird extends ApplicationAdapter {
 
         }
 
+        batch.end();
 
         //shapeRenderer.end();
 
