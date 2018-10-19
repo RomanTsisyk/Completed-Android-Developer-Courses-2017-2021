eebfd2a1992550a96025e2e1b25123ee14d97881
romantsisyk <romantsisyk1@gmail.com>
2018-10-19 21:39:20 +0200

    finishing SharedPrefs Repository

--- app/src/main/java/com/raywenderlich/android/datadrop/model/SharedPrefsRepositpry.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/SharedPrefsRepositpry.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.android.datadrop.model
 
 import android.content.Context
 import android.provider.ContactsContract
+import com.google.gson.Gson
 import com.raywenderlich.android.datadrop.app.DataDropApplication
 
 object SharedPrefsRepositpry : DropRepository {
@@ -9,20 +10,29 @@ object SharedPrefsRepositpry : DropRepository {
 
     private const val SHARED_PREFS_REPOSITORY = "SHARED_PREFS_REPOSITORY"
 
-    fun sharedPrefs() = DataDropApplication.getAppContext()
+    private val gson = Gson()
+
+    private fun sharedPrefs() = DataDropApplication.getAppContext()
             .getSharedPreferences(SHARED_PREFS_REPOSITORY, Context.MODE_PRIVATE)
 
     override fun addDrop(drop: Drop) {
+        sharedPrefs().edit().putString(drop.id, gson.toJson(drop)).apply()
     }
 
     override fun getDrops(): List<Drop> {
-        return emptyList()
+        return sharedPrefs().all.keys
+                .map { sharedPrefs().getString( it, "") }
+                .filterNot { it.isNullOrBlank() }
+                .map { gson.fromJson(it, Drop::class.java) }
     }
 
     override fun clearDrop(drop: Drop) {
+        sharedPrefs().edit().remove(drop.id).apply()
     }
 
     override fun clearAllDrops() {
+        sharedPrefs().edit().clear().apply()
+
     }
 
 }
