ed52e18083578573a88a5f396e6deedc910ccd2c
winnthannhtike <winnthannhtike@gmail.com>
2018-10-28 10:22:13 +0630

    first commit

---
+++ app/src/main/java/com/raywenderlich/android/moviepager/MovieDatabase.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.moviepager
+
+import android.arch.persistence.db.SupportSQLiteDatabase
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+@Database(entities = [(Movie::class)], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+  abstract fun movieDao(): MovieDao
+
+  companion object {
+    private const val TAG = "MovieDatabase"
+    private var instance: MovieDatabase? = null
+
+    @Synchronized
+    fun get(context: Context): MovieDatabase {
+      if (instance == null) {
+        instance = Room.databaseBuilder(context.applicationContext,
+            MovieDatabase::class.java, "MovieDatabase")
+            .addCallback(object : RoomDatabase.Callback() {
+              override fun onCreate(db: SupportSQLiteDatabase) {
+                fillInDatabase(context.applicationContext)
+              }
+            }).build()
+      }
+      return instance!!
+    }
+
+    private fun fillInDatabase(context: Context) {
+      bgThread {
+        val jsonString = readJson(context)
+
+        if (jsonString != null) {
+          val movieData = Gson().fromJson(jsonString, MovieData::class.java)
+          get(context).movieDao().insert(movieData.movies)
+          Log.v(TAG, movieData.toString())
+        }
+      }
+    }
+
+    private fun readJson(context: Context): String? {
+      var json: String? = null
+
+      try {
+        val inputStream = context.assets.open("movie_data.json")
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        json = String(buffer, StandardCharsets.UTF_8)
+      } catch (e: IOException) {
+        Log.e(TAG, "Error reading JSON data ::: " + e.message, e)
+      }
+
+      return json
+    }
+  }
+}
---
+++ app/src/main/java/com/raywenderlich/android/moviepager/MovieListActivity.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.moviepager
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_movie_list.*
+
+class MovieListActivity : AppCompatActivity() {
+
+  private val viewModel by lazy(LazyThreadSafetyMode.NONE) {
+    ViewModelProviders.of(this).get(MovieListViewModel::class.java)
+  }
+
+  private val adapter = MovieListAdapter(emptyList())
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_movie_list)
+
+    movieRecyclerView.adapter = adapter
+
+    viewModel.allMovies.observe(this, Observer { movies ->
+      if (movies != null) {
+        adapter.updateMovies(movies)
+      }
+    })
+
+    setupSwipeToDelete()
+  }
+
+  private fun setupSwipeToDelete() {
+    ItemTouchHelper(object : ItemTouchHelper.Callback() {
+      override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int =
+          makeMovementFlags(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+
+      override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                          target: RecyclerView.ViewHolder): Boolean = false
+
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
+        (viewHolder as? MovieListAdapter.MovieViewHolder)?.movie?.let {
+          viewModel.remove(it)
+        }
+      }
+    }).attachToRecyclerView(movieRecyclerView)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    super.onCreateOptionsMenu(menu)
+    menuInflater.inflate(R.menu.menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.goto_top -> scrollToTop()
+      R.id.goto_bottom -> scrollToBottom()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun scrollToTop() {
+    movieRecyclerView.scrollToPosition(0)
+  }
+
+  private fun scrollToBottom() {
+    movieRecyclerView.scrollToPosition(movieRecyclerView.adapter.itemCount - 1)
+  }
+}
