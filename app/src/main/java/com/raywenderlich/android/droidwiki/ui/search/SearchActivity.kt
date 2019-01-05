29aea732f5df5639f76192fde45fb3661e280cbe
romantsisyk <romantsisyk1@gmail.com>
2019-01-05 15:05:47 +0100

    di SearchActivity by EntryPresenter

--- app/src/main/java/com/raywenderlich/android/droidwiki/ui/search/SearchActivity.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/ui/search/SearchActivity.kt
@@ -41,11 +41,13 @@ import com.raywenderlich.android.droidwiki.model.Entry
 import kotlinx.android.synthetic.main.activity_search.*
 import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.raywenderlich.android.droidwiki.application.WikiApplication
 import com.raywenderlich.android.droidwiki.utils.errorDialog
+import javax.inject.Inject
 
 class SearchActivity : Activity(), EntryView {
 
-  private val presenter: EntryPresenter = EntryPresenterImpl()
+  @Inject lateinit var presenter: EntryPresenter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -55,6 +57,9 @@ class SearchActivity : Activity(), EntryView {
     actionBar?.setDisplayHomeAsUpEnabled(true)
 
     results_rv.layoutManager = LinearLayoutManager(this)
+
+    (application as WikiApplication).wikiComponent.inject(this)
+
 
     presenter.setView(this)
 
