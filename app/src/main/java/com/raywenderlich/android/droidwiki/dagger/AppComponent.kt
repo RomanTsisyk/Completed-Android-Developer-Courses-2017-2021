03cdf8a92c2479dc8c5e38c3fc1543ce02038dc9
romantsisyk <romantsisyk1@gmail.com>
2019-01-05 21:59:08 +0100

    Network injection

--- app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
@@ -5,9 +5,14 @@ import com.raywenderlich.android.droidwiki.ui.search.SearchActivity
 import dagger.Component
 import javax.inject.Singleton
 
+
 @Singleton
-@Component(modules = [AppModule::class, PresenterModule::class])
-interface AppComponent{
-    fun inject( target: HomepageActivity)
-    fun inject( target: SearchActivity)
+@Component(modules = [
+    AppModule::class,
+    PresenterModule::class,
+    NetworkModule::class,
+    WikiModule::class])
+interface AppComponent {
+    fun inject(target: HomepageActivity)
+    fun inject(target: SearchActivity)
 }
