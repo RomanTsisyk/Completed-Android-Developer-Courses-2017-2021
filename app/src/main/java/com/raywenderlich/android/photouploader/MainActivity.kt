56898090d4cf8e1a6879b7b74a62fbe83f01cba3
romantsisyk <romantsisyk1@gmail.com>
2019-01-12 21:06:33 +0100

    Create CleanFilesWorker and UploadWorker

--- app/src/main/java/com/raywenderlich/android/photouploader/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/photouploader/MainActivity.kt
@@ -43,136 +43,140 @@ import android.view.View
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
-import com.raywenderlich.android.photouploader.wockers.CompressWorker
-import com.raywenderlich.android.photouploader.wockers.FilterWorker
-import com.raywenderlich.android.photouploader.wockers.KEY_IMAGE_INDEX
-import com.raywenderlich.android.photouploader.wockers.KEY_IMAGE_URI
+import com.raywenderlich.android.photouploader.workers.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val GALLERY_REQUEST_CODE = 300
-        private const val PERMISSIONS_REQUEST_CODE = 301
+  companion object {
+    private const val GALLERY_REQUEST_CODE = 300
+    private const val PERMISSIONS_REQUEST_CODE = 301
 
-        private const val MAX_NUMBER_REQUEST_PERMISSIONS = 2
+    private const val MAX_NUMBER_REQUEST_PERMISSIONS = 2
 
-        private const val IMAGE_TYPE = "image/*"
-        private const val IMAGE_CHOOSER_TITLE = "Select Picture"
+    private const val IMAGE_TYPE = "image/*"
+    private const val IMAGE_CHOOSER_TITLE = "Select Picture"
 
-        private val PERMISSIONS = arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+  }
+
+  private var permissionRequestCount: Int = 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    initUi()
+
+    requestPermissionsIfNecessary()
+  }
+
+  private fun initUi() {
+    uploadGroup.visibility = View.GONE
+
+    pickPhotosButton.setOnClickListener { showPhotoPicker() }
+  }
+
+  private fun showPhotoPicker() {
+    val intent = Intent().apply {
+      type = IMAGE_TYPE
+      putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+      action = Intent.ACTION_OPEN_DOCUMENT
     }
 
-    private var permissionRequestCount = 0
+    startActivityForResult(Intent.createChooser(intent, IMAGE_CHOOSER_TITLE), GALLERY_REQUEST_CODE)
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initUi()
+  private fun requestPermissionsIfNecessary() {
+    if (!hasRequiredPermissions()) {
+      askForPermissions()
+    }
+  }
 
-        requestPermissionsIfNecessary()
+  private fun askForPermissions() {
+    if (permissionRequestCount < MAX_NUMBER_REQUEST_PERMISSIONS) {
+      permissionRequestCount += 1
+
+      ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+    } else {
+      pickPhotosButton.isEnabled = false
+    }
+  }
+
+  private fun hasRequiredPermissions(): Boolean {
+    val permissionResults = PERMISSIONS.map { permission ->
+      ContextCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED
     }
 
-    private fun initUi() {
-        uploadGroup.visibility = View.GONE
+    return permissionResults.all { isGranted -> isGranted }
+  }
 
-        pickPhotosButton.setOnClickListener { showPhotoPicker() }
+  override fun onRequestPermissionsResult(
+      requestCode: Int,
+      permissions: Array<String>,
+      grantResults: IntArray) {
+
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (requestCode == PERMISSIONS_REQUEST_CODE) {
+      requestPermissionsIfNecessary()
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (data != null && resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
+
+      val applySepiaFilter = buildSepiaFilterRequests(data)
+      val zipFiles = OneTimeWorkRequest.Builder(CompressWorker::class.java).build()
+
+      val uploadZip = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+          .build()
+
+      val cleanFiles = OneTimeWorkRequest.Builder(CleanFilesWorker::class.java).build()
+
+      val workManager = WorkManager.getInstance()
+
+      workManager.beginWith(cleanFiles)
+          .then(applySepiaFilter)
+          .then(zipFiles)
+          .then(uploadZip)
+          .enqueue()
+    }
+  }
+
+  private fun buildSepiaFilterRequests(intent: Intent): List<OneTimeWorkRequest> {
+    val filterRequests = mutableListOf<OneTimeWorkRequest>()
+
+    intent.clipData?.run {
+      for (i in 0 until itemCount) {
+        val imageUri = getItemAt(i).uri
+
+        val filterRequest = OneTimeWorkRequest.Builder(FilterWorker::class.java)
+            .setInputData(buildInputDataForFilter(imageUri, i))
+            .build()
+        filterRequests.add(filterRequest)
+      }
     }
 
-    private fun showPhotoPicker() {
-        val intent = Intent().apply {
-            type = IMAGE_TYPE
-            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            action = Intent.ACTION_OPEN_DOCUMENT
-        }
 
-        startActivityForResult(Intent.createChooser(intent, IMAGE_CHOOSER_TITLE), GALLERY_REQUEST_CODE)
+    intent.data?.run {
+      val filterWorkRequest = OneTimeWorkRequest.Builder(FilterWorker::class.java)
+          .setInputData(buildInputDataForFilter(this, 0))
+          .build()
+
+      filterRequests.add(filterWorkRequest)
     }
 
-    private fun requestPermissionsIfNecessary() {
-        if (!hasRequiredPermissions()) {
-            askForPermissions()
-        }
+    return filterRequests
+  }
+
+  private fun buildInputDataForFilter(imageUri: Uri?, index: Int): Data {
+    val builder = Data.Builder()
+    if (imageUri != null) {
+      builder.putString(KEY_IMAGE_URI, imageUri.toString())
+      builder.putInt(KEY_IMAGE_INDEX, index)
     }
-
-    private fun askForPermissions() {
-        if (permissionRequestCount < MAX_NUMBER_REQUEST_PERMISSIONS) {
-            permissionRequestCount += 1
-
-            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE)
-        } else {
-            pickPhotosButton.isEnabled = false
-        }
-    }
-
-    private fun hasRequiredPermissions(): Boolean {
-        val permissionResults = PERMISSIONS.map { permission ->
-            ContextCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED
-        }
-
-        return permissionResults.all { isGranted -> isGranted }
-    }
-
-    override fun onRequestPermissionsResult(
-            code: Int,
-            permissions: Array<String>,
-            result: IntArray) {
-        super.onRequestPermissionsResult(code, permissions, result)
-        if (code == PERMISSIONS_REQUEST_CODE) {
-            requestPermissionsIfNecessary()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (data != null
-                && resultCode == Activity.RESULT_OK
-                && requestCode == GALLERY_REQUEST_CODE) {
-
-            val applySepiaFilter = buildSepiaFilterRequests(data)
-
-            val zipFiles = OneTimeWorkRequest.Builder(CompressWorker::class.java).build()
-            val workManager = WorkManager.getInstance()
-            workManager.beginWith(applySepiaFilter)
-                    .then(zipFiles)
-                    .enqueue()
-        }
-    }
-
-    private fun buildInputDataForFilter(imageUri: Uri?, index: Int): Data {
-        val builder = Data.Builder()
-
-        if (imageUri != null) {
-            builder.putString(KEY_IMAGE_URI, imageUri.toString())
-            builder.putInt(KEY_IMAGE_INDEX, index)
-        }
-        return builder.build()
-    }
-
-    private fun buildSepiaFilterRequests(intent: Intent): List<OneTimeWorkRequest> {
-        val filterRequests = mutableListOf<OneTimeWorkRequest>()
-
-        intent.clipData?.run {
-            for (i in 0 until itemCount) {
-                val imageUri = getItemAt(i).uri
-
-                val filterRequest = OneTimeWorkRequest.Builder(FilterWorker::class.java)
-                        .setInputData(buildInputDataForFilter(imageUri, i))
-                        .build()
-                filterRequests.add(filterRequest)
-            }
-        }
-        intent.data?.run {
-            val filterRequest = OneTimeWorkRequest.Builder(FilterWorker::class.java)
-                    .setInputData(buildInputDataForFilter(this, 0))
-                    .build()
-
-            filterRequests.add(filterRequest)
-        }
-        return filterRequests
-
-    }
+    return builder.build()
+  }
 }
