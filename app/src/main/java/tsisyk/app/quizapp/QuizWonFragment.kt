f85c17c37789b0a02945d6a60bf59f777e0297ea
formycv <trmstudio8@gmail.com>
2019-07-05 02:15:27 +0300

    let`s go

---
+++ app/src/main/java/tsisyk/app/quizapp/QuizWonFragment.kt
@@ -0,0 +1,20 @@
+package tsisyk.app.quizapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import tsisyk.app.quizapp.databinding.FragmentGameWonBinding
+
+
+class QuizWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+}
