e6d6a737c5808145166fd381db10e2883723fc3a
RomanTsisyk <RomanTsisyk1@gmail.com>
2019-07-20 21:48:38 +0300

    java commit

---
+++ app/src/main/java/tsisyk/app/trackmysleepquality/MainActivity.kt
@@ -0,0 +1,13 @@
+package tsisyk.app.trackmysleepquality
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/tsisyk/app/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -0,0 +1,1 @@
+package tsisyk.app.trackmysleepquality.sleeptracker
