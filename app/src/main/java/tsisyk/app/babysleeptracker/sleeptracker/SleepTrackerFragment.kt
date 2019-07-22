249063c1f9350794af4249676f713bf2ea7c7627
romantsisyk <romantsisyk1@gmail.com>
2019-07-22 04:53:19 +0300

    Adding a ViewModel

--- app/src/main/java/tsisyk/app/babysleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/tsisyk/app/babysleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,17 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import tsisyk.app.babysleeptracker.R
+import tsisyk.app.babysleeptracker.database.SleepDatabase
 import tsisyk.app.babysleeptracker.databinding.FragmentSleepTrackerBinding
 
 
 class SleepTrackerFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_sleep_tracker, container, false)
+            inflater, R.layout.fragment_sleep_tracker, container, false
+        )
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel = ViewModelProviders.of(
+            this, viewModelFactory
+        ).get(SleepTrackerViewModel::class.java)
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
